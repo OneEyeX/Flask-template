562f5eadc587c64ae0beeb6b5f262a01ad72e738
Chedly Chahed <chedly199@gmail.com>
2022-06-26 03:33:02 +0100

    first commit

---
+++ website/static/index.js
@@ -0,0 +1,9 @@
+// to delete note
+function deleteNote(noteId) {
+  fetch("/delete-note", {
+    method: "POST",
+    body: JSON.stringify({ noteId: noteId }),
+  }).then((_res) => {
+    window.location.href = "/";
+  });
+}
